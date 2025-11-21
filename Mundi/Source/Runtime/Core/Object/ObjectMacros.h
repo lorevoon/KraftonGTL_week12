7dc1e903e2c6baf015d0547cba73eb2aece7cf3f
nansu0425 <nansu0425@gmail.com>
2025-11-21 21:24:47 +0900

    fix: 몇 개의 경고 제거

--- Mundi/Source/Runtime/Core/Object/ObjectMacros.h
+++ Mundi/Source/Runtime/Core/Object/ObjectMacros.h
@@ -208,19 +208,6 @@ struct TPropertyTypeTraits
 		Class->AddProperty(Prop); \
 	}
 
-#define ADD_PROPERTY_CURVE(VarType, VarName, CategoryName, bEditAnywhere, ...) \
-	{ \
-		static_assert(std::is_array_v<std::remove_reference_t<decltype(CategoryName)>>, \
-		              "CategoryName must be a string literal!"); \
-		FProperty Prop; \
-		Prop.Name = #VarName; \
-		Prop.Type = EPropertyType::Curve; \
-		Prop.Offset = offsetof(ThisClass_t, VarName); \
-		Prop.Category = CategoryName; \
-		Prop.bIsEditAnywhere = bEditAnywhere; \
-		Prop.Tooltip = "" __VA_ARGS__; \
-		Class->AddProperty(Prop); \
-	}
 #define ADD_PROPERTY_COUNT(VarType, VarName, CategoryName, bEditAnywhere, ...) \
 	{ \
 		static_assert(std::is_array_v<std::remove_reference_t<decltype(CategoryName)>>, \
